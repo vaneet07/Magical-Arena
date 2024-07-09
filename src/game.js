199322f57f0d610ac8c850c2dcecb08e9f2cf4e8
vaneet07 <gargvaneet7@gmail.com>
2024-07-09 23:10:29 +0530

    added game start function in game class

--- src/game.js
+++ src/game.js
@@ -4,7 +4,21 @@ const Arena = require('./arena');
 
 class Game {
     constructor(player1, player2) {
-        this.players = [player1, player2].sort((a, b) => a.health - b.health);
+        this.players = [player1, player2].sort((a, b) => a.health - b.health); // SORTING PLAYERS ACCORDING TO THEIR HEALTH
+    }
+
+    play() {
+        console.log("Game starts!");
+        let round = 1;
+
+        while (this.players[0].isAlive() && this.players[1].isAlive()) {
+            console.log(`\nRound ${round}`);
+            this.playRound();
+            round++;
+        }
+
+        const winner = this.players[0].isAlive() ? this.players[0] : this.players[1];
+        console.log(`\n${winner.name} wins the game!`);
     }
 }
 
