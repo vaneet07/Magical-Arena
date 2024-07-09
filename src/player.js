e72da58ea6190529d73f98f66f6a5b868d062991
vaneet07 <gargvaneet7@gmail.com>
2024-07-10 01:25:02 +0530

    update player class with retrieving current stats functionality

--- src/player.js
+++ src/player.js
@@ -5,13 +5,17 @@ class Player {
         this.strength = strength;
         this.attack = attack;
     }
-    
+
     isAlive() { // CHECK IF PLAYER IS ALIVE OR DEAD
         return this.health > 0;
     }
 
     takeDamage(damage) { // REDUCES HEALTH OF PLAYER BY DAMAGE TAKEN
         this.health = Math.max(0, this.health - damage);
+    }
+
+    getStats() { // GET PLAYER STATS
+        return `${this.name} (Health: ${this.health}, Strength: ${this.strength}, Attack: ${this.attack})`;
     }
 }
 
