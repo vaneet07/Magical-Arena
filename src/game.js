b1c29545096b559322817716ded0ba134b514bc3
vaneet07 <gargvaneet7@gmail.com>
2024-07-09 23:08:46 +0530

    added players to game entity

--- src/game.js
+++ src/game.js
@@ -3,7 +3,9 @@ const Player = require('./player');
 const Arena = require('./arena');
 
 class Game {
-    
+    constructor(player1, player2) {
+        this.players = [player1, player2].sort((a, b) => a.health - b.health);
+    }
 }
 
 module.exports = Game;
