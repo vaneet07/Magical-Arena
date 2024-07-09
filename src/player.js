270facb46a839508883a39bdfdaa542da347ef5c
vaneet07 <gargvaneet7@gmail.com>
2024-07-09 22:44:13 +0530

    Created basic player entity with attributes

---
+++ src/player.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor(name, health, strength, attack) { //PLAYER ATTRIBUTES
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+}
+
+module.exports = Player;
