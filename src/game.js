386423e1a84e968f651db8aa005647d6bac1d8fa
vaneet07 <gargvaneet7@gmail.com>
2024-07-09 23:12:20 +0530

    added game fight round functions to game class

--- src/game.js
+++ src/game.js
@@ -7,7 +7,7 @@ class Game {
         this.players = [player1, player2].sort((a, b) => a.health - b.health); // SORTING PLAYERS ACCORDING TO THEIR HEALTH
     }
 
-    play() {
+    play() { // FUNCTION TO START THE GAME
         console.log("Game starts!");
         let round = 1;
 
@@ -19,6 +19,22 @@ class Game {
 
         const winner = this.players[0].isAlive() ? this.players[0] : this.players[1];
         console.log(`\n${winner.name} wins the game!`);
+    }
+
+    playRound() { // FUNCTION TO PLAY GAME ROUND WISE
+        for (let i = 0; i < 2; i++) {
+            const attacker = this.players[i];
+            const defender = this.players[1 - i];
+
+            if (!defender.isAlive()) break;
+
+            const result = Arena.fight(attacker, defender);
+
+            console.log(`${attacker.name} attacks ${defender.name}`);
+            console.log(`Attack roll: ${result.attackerRoll}, Defense roll: ${result.defenderRoll}`);
+            console.log(`Damage: ${result.damage}, Defense: ${result.defense}, Net damage: ${result.netDamage}`);
+            console.log(`${defender.name}'s health: ${defender.health}`);
+        }
     }
 }
 
