1c0ac4457c033d00e6e27d5a679cb9827ddd429c
vaneet07 <gargvaneet7@gmail.com>
2024-07-10 01:15:18 +0530

    update main.js to use async/wait

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 const Player = require('./player');
 const Game = require('./game');
 
-const playerA = new Player("Player A", 50, 5, 10);
-const playerB = new Player("Player B", 100, 10, 5);
+async function runGame() {
+    const playerA = new Player("Player A", 50, 5, 10);
+    const playerB = new Player("Player B", 100, 10, 5);
 
-const game = new Game(playerA, playerB);
-game.play();
+    const game = new Game(playerA, playerB);
+    await game.play();
+}
+
+runGame();
