a37328df1775884645e91eb5aa8df121bfd0da02
vaneet07 <gargvaneet7@gmail.com>
2024-07-10 20:37:57 +0530

    Updated main.js to run the game

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-const Player = require('./player');
 const Game = require('./game');
 
 async function runGame() {
-    const playerA = new Player("Player A", 50, 5, 10);
-    const playerB = new Player("Player B", 100, 10, 5);
-
-    const game = new Game(playerA, playerB);
+    const game = new Game();
     await game.start();
 }
 
